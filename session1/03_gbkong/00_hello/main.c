db2f164b2e343d05b5df93b19f2033c7a649a244
GyeongbinKong <rudqls96@gmail.com>
2022-09-26 13:01:06 +0900

    test

--- session1/03_gbkong/00_hello/main.c
+++ session1/03_gbkong/00_hello/main.c
@@ -4,12 +4,7 @@
 extern int increment(int i);
 
 int main() {
-<<<<<<< HEAD
-    int i = 13;
-    printf("Hello, world! %d\n", increment(i));
-=======
-    int i=10;
-    printf("Hello, world! Nikki %d\n", increment(i));
->>>>>>> c4c3c9c31b836c2d0c6b97a37d9c6747475fa0ab
+    int i = 10;
+    printf("Hello, world! Gyeongbin %d\n", increment(i));
     return 0;
 }
