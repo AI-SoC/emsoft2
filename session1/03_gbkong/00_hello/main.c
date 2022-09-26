14308cf84e24cc2476e003e73922cfe2307b5c7f
GyeongbinKong <rudqls96@gmail.com>
2022-09-26 11:53:36 +0900

    test

---
+++ session1/03_gbkong/00_hello/main.c
@@ -0,0 +1,15 @@
+// hello.c
+#include <stdio.h>
+
+extern int increment(int i);
+
+int main() {
+<<<<<<< HEAD
+    int i = 13;
+    printf("Hello, world! %d\n", increment(i));
+=======
+    int i=10;
+    printf("Hello, world! Nikki %d\n", increment(i));
+>>>>>>> c4c3c9c31b836c2d0c6b97a37d9c6747475fa0ab
+    return 0;
+}
